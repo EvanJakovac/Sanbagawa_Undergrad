c2d910304bb6bd0850946aaf2780e73c575c50e3
EvanJakovac <edj5098@psu.edu>
2025-05-02 14:56:33 -0400

    Add files via upload

---
+++ No_intercepts_check.m
@@ -0,0 +1,155 @@
+%% No_intercepts_check 
+% Rellie M. Goddard, July 2020
+
+% This function checks the number of intercepts required to acurately capture the subgrain-size
+
+% Required functions:
+% *ProcessEBSD_fun.m
+% *LinearIntercepts_fun.m
+
+% Required user inputs:
+% * nx_max: The maximum number of intercepts that you want to test (recommended to start at either 30 or 40)
+% * gb_min: Minimum misorientation angle to define a grain boundary in 
+%       degrees. Used for constructing maps
+% * sg_min: Minimum misorientation angle to define a subgrain boundary in 
+%       degrees. Only used for constructing maps.
+% * cutoff: Minimum misorientation angle to define a subgrain boundary in
+%       degrees. Used for piezometer calculations. Recommended value is 1.
+% * phase: Name of the phase of interest (e.g., 'Forsterite')
+% * crystal: Crystal system of the phase to be examined (e.g., 'orthorhombic')
+% * test: When set to 1, reduces the size of the input EBSD map by taking
+%       every tenth pixel in both the horizontal and vertical direction. Can be 
+%       utilized to ensure the script runs correctly for a new sample file or for
+%       troubleshooting. During full analysis, test should be set to 0.
+% * include_low: The choice of if you want to test very low number of intercepts (1-9). To include set to 1. Othewise, set to 0.
+%
+%% Additional user inputs produced by MTEX
+% * CS: Crystal symmetry class variable for all indexed phases in EBSD map.
+% * pname: Path to data (e.g., 'C:/Users/admin/data/')
+% * fname: File name combined with path
+% 
+% Results: 
+%   Figures showing the line intercepts on top of EBSD data for each iteration, a figure showing the mean line intercept length against the no. of
+%   intercepts, and a figure showing the change in mean line intercept length relative to last
+%
+% The test is successful if the measured mean line-intercept length stabilises (change in intercept length relative to last is < or = 2.5%). 
+% If the mean line-intercept does not stabilise, increase nx_max and run again. 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Data import
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+close, clear all 
+
+% USER INPUT: Data inport information from MTEX
+% This information is produced automatically by the MTEX import wizard 
+% Paste in your CS, plotting conventions, pname, and fname here. 
+
+% Specify Crystal and Specimen Symmetries 
+% crystal symmetry
+CS =  {};
+% plotting convention
+setMTEXpref('xAxisDirection','east');
+setMTEXpref('zAxisDirection','outOfPlane');
+
+% Specify File Names 
+% path to file 
+pname = 'yourPath';
+% which file to be imported  
+fname = [pname 'yourFileName.ctf'];
+
+%% USER INPUT: Required information 
+nx_max = [30]; % Max number of intercepts to try. Must be multiple of 10. Suggested starting value, 30 or 40
+gb_min = [10]; % Minimum misorientation for grain boundary (for figures)
+sg_min = [1]; % Minimum misorientation for subgrain boundary (for figures)
+cutoff = [1]; % Minimum misorientation for subgrain boundary (for calculation)
+phase = 'Quartz'; % Phase to measure. Must match a phase present in CS.
+crystal = '-3m1'; % Crystal system of phase to measure. 
+test = 0; % Set to 1 to speed up analysis when troubleshooting. 
+include_low = 0; %to include analysis, set to 1. Otherwise, set to 0
+
+%% END OF USER INPUTS 
+
+%% Calculate and plot
+
+if include_low % create an array to keep data in
+    X1 = [1:1:9]';
+    X2 = 10.*[1:1:(nx_max/10)]';
+    X = [X1;X2];
+    X = X';
+    Y = zeros([1,((nx_max/10) + 9)]);
+else
+    X = 10.*[1:1:(nx_max/10)];
+    Y = zeros([1, nx_max/10]);
+end
+
+
+%  Call on the ProcessEBSD function. 
+[ebsd,grains,subgrains] = ProcessEBSD_fun(fname,gb_min,sg_min, CS, test, 0, 0);
+
+if include_low
+    for nx = 1:1:9
+        ny = nx; 
+        [lengths_x, lengths_y] = LinearIntercepts_fun(ebsd,nx,ny,cutoff,phase,crystal);
+        d_h = lengths_x;
+        d_v = lengths_y;
+        d = [d_h;d_v];
+        XY = sum(d)/length(d);
+        Y(nx) = XY;
+
+        
+        
+    end 
+
+    for nx= 10:10:nx_max
+        ny = nx;
+        [lengths_x, lengths_y] = LinearIntercepts_fun(ebsd,nx,ny,cutoff,phase,crystal);
+        d_h = lengths_x;
+        d_v = lengths_y;
+        d = [d_h;d_v];
+        XY = sum(d)/length(d);
+        nx = 9 + (nx/10)
+        Y(nx) = XY;
+    end 
+else 
+
+    for nx= 10:10:nx_max
+        ny = nx;
+        [lengths_x, lengths_y] = LinearIntercepts_fun(ebsd,nx,ny,cutoff,phase,crystal);
+        d_h = lengths_x;
+        d_v = lengths_y;
+        d = [d_h;d_v];
+        XY = sum(d)/length(d);
+        nx = (nx/10)
+        Y(nx) = XY;
+    end 
+end 
+    
+
+% Plot figure 
+    figure
+    scatter(X,Y,40, 'filled', 'r');
+    hold on 
+    %Set axis limits 
+    yl = ylim; % Get current limits.
+    ylim([0, 2*yl(2)]);  
+    set(0,'DefaultAxesFontSize',15,'DefaultAxesFontName','helvetica','DefaultAxesTickLength',[.03 .02],'DefaultAxesLineWidth',2);
+    
+    %Label the graph
+    ylabel('Mean intercept length  (\mum)', 'FontSize', 15);
+    xlabel('No. Intercepts ', 'FontSize', 15);
+    box on 
+    
+    figure
+    Y_change = zeros(1,(length(X)-1));
+    for i = 2:1:(length(X))
+        Y_change(i-1) = (abs(Y(i)-Y(i-1))/Y(i-1))*100;
+    end 
+    X_change = (X(2:length(X)));
+    scatter(X_change,Y_change,40, 'filled', 'k');
+    %Set format 
+    set(0,'DefaultAxesFontSize',15,'DefaultAxesFontName','helvetica','DefaultAxesTickLength',[.03 .02],'DefaultAxesLineWidth',2);
+    %Label the graph
+    ylabel('Change relative to previous (%)', 'FontSize', 15);
+    xlabel('No. Intercepts ', 'FontSize', 15);
+    box on 
+    
---
+++ Undersampling_Step_Size.m
@@ -0,0 +1,109 @@
+%% Undersampling_Step_size - tests if the spatial resolution is high enough 
+% Rellie M. Goddard, July 2020
+
+% This function examines how the effective step-size of EBSD maps affects
+% the mean line intercept length of a given phase in the map.
+
+%% Required functions:
+% * ProcessEBSD_fun.m
+% * LinearIntercepts_fun.m
+% * Undersampling.fun.m
+
+%% Required user inputs:
+% * nx: The number of intercept lines, chosen based on analysis from 
+%       No_intercepts_check.m.
+% * Int_max: The number of times you want to increase the step-size. The 
+%       step-size at an iteration will be Int_max multiplied by the original
+%       step-size. For each iteration a .ctf file will be created. 
+% * Image_title: Title for figure 
+% * Header_size: Number of lines, up to and including the line starting with
+%       "phase" in the .ctf file (as seen if opened in a text editor)
+% * gb_min: Minimum misorientation angle to define a grain boundary in 
+%       degrees. Used for constructing maps
+% * sg_min: Minimum misorientation angle to define a subgrain boundary in 
+%       degrees. Only used for constructing maps.
+% * cutoff: Minimum misorientation angle to define a subgrain boundary in
+%       degrees. Used for piezometer calculations. Recommended value is 1.
+% * phase: Name of the phase of interest (e.g., 'Forsterite')
+% * crystal: Crystal system of the phase to be examined (e.g., 'orthorhombic')
+% * test: When set to 1, reduces the size of the input EBSD map by taking
+%       every tenth pixel in both the horizontal and vertical direction. Can be 
+%       utilized to ensure the script runs correctly for a new sample file or for
+%       troubleshooting. During full analysis, test should be set to 0.
+% * Phase_map: To output a phase map, set to 1. Othewise, set to 0.
+% * Band_contrast: To output a band contrast map, set to 1. Otherwise, set
+%       to 0.
+%
+%% Additional user inputs produced by MTEX
+% * CS: Crystal symmetry class variable for all indexed phaes in EBSD map.
+% * pname: Path to data (e.g., 'C:/Users/admin/data/')
+% * fname: File name combined with path
+%
+%% Results
+% A figure showing the Intercept Variation Factor plotted against the number 
+% of pixels per intercept will be produced. See Goddard et al., (submitted) for an
+% explanation of the variables. 
+%
+% The test is considered successful if the measured mean intercept length
+% is not sensitive to the effective step size. The presence of an asymptote
+% at an intercept variation factor of 1 is evidence that step size is small
+% enough to capture the mean intercept length. If such an asymptote doesn't
+% exist then either re-map the sample, using a smaller step size, or use the 
+% subgrain-size stress measurement as a lower bound.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Data import
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear, close all 
+
+%% USER INPUT: Data import information from MTEX
+% This information is produced automatically by the MTEX import wizard 
+% Paste in your CS, plotting conventions, pname, and fname here.
+
+% Specify Crystal and Specimen Symmetries 
+% crystal symmetry
+CS =  {'-3m1'};
+% plotting convention
+setMTEXpref('xAxisDirection','east');
+setMTEXpref('zAxisDirection','outOfPlane');
+
+% Specify File Names 
+% path to file 
+%pname = 'yourPath';
+% which file to be imported 
+%fname = [pname 'yourFileName.ctf'];
+
+
+%% USER INPUT: Required information 
+nx = [30]; % Number of intercept lines 
+Int_max = [2]; % Number of times to increase the step-size
+title_text = 'yourTitle'; % Title for figures 
+header_size = [19]; % Number of rows in header of CTF 
+gb_min = [10]; % Minimum misorientation for grain boundary (for figures)
+sg_min = [1]; % Minimum misorientation for subgrain boundary (for figures)
+cutoff = [1]; % Minimum misorientation for subgrain boundary (for calculation)
+phase = 'Quartz'; % Phase to measure. Must match a phase present in CS.
+crystal = '-3m1'; % Crystal system of phase to measure. 
+Phase_map = 0; % Set to 1 to plot a phase map of the EBSD data. 
+Band_contrast = 0; % Set to 1 to plot a band contrast map.
+test = 0; % Set to 1 to speed up analysis when troubleshooting. 
+
+%% END OF USER INPUTS 
+
+%% Programmatically calculate other necessary variables 
+temp = split(fname, {'/','.'});
+sampname = temp{end-1}; % Extract file name with no extension 
+ny = nx; % Set number of intercepts in y-direction to equal number of intercepts in the x-direction.
+
+%% Calculate and plot 
+[fname_new, stepx_all, Step_size_SG_size] = undersampling_fun(Int_max, sampname, header_size,gb_min,sg_min,test, Phase_map, Band_contrast, nx, ny, cutoff, phase, crystal, CS);
+
+close all 
+
+figure 
+    plot(Step_size_SG_size(1)./stepx_all, Step_size_SG_size/Step_size_SG_size(1),'-o', 'LineWidth', 3, 'color', [0.49 0.34 0.75], 'MarkerSize', 7);
+    set(0,'DefaultAxesFontSize',15,'DefaultAxesFontName','helvetica','DefaultAxesTickLength',[.03 .02],'DefaultAxesLineWidth',2);
+    set(gca,'fontsize',15);
+    ylabel('Intercept variation factor ,\bf\lambda\rm/\bf\lambda\rm_b_e_s_t', 'FontSize',15);
+    xlabel('Pixels per intercept, \bf\lambda\rm_b_e_s_t/step size', 'FontSize', 15);
+    title(title_text, 'FontSize', 15)
